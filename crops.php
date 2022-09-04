860a26e07c319a980948203e74ac2690a0cfc516
maheswarivallem <104861478+maheswarivallem@users.noreply.github.com>
2022-09-04 21:39:41 +0530

    Add files via upload

---
+++ crops.php
@@ -0,0 +1,40 @@
+<DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="crops.css">
+</head>
+<body>
+   <div class="mahi">
+     <h1>Heading for project</h1>
+     <div class="grid-container">
+        <div class="grid-item">Summer</div>
+        <div class="grid-item">Winter</div>
+        <div class="grid-item">Rain</div>  
+        <div class="grid-item">
+            <img src="th7.jpg"/>
+        </div>
+        <div class="grid-item">
+            <img src="th 8.jpg"/>
+        </div>
+        <div class="grid-item">
+            <img src="th4.jpg"/>
+        </div>
+        <div class="grid-item">
+            <p>Crops grown in summer are Paddy, Corn, etc. Other examples of Summer crops are Pumpkin, Cucumber, Bitter Rice, Corn, Cucumber, Melon, Pepper, Tomato, and some coarse Cereals sown during March and are generally harvested by June end.</p>
+        </div>
+        <div class="grid-item">
+            <p>Broccoli,Cabbage,Spinach,Green Pea,Collard, Garlic, Allium.</p>
+        </div>
+        <div class="grid-item">
+            <p>Cucumber,Tomato, Radish, Beans, Green Chillies, Brinjal.</p>
+        </div>
+ 
+     </div>
+    
+     
+      
+      
+
+    </div>
+</body>
+</html>
---
+++ user.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="mahi">
+       <h1>Welcome to  Project</h1>
+       <button type="button" class="btn">
+            <a href="crops.php">
+              Click here
+            </a>
+       </button>
+      
+
+    </div>
+</body>
+</html>
